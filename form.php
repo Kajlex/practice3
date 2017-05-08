30191f3827baa970d2cc6f18c66454c315bc85de
Kailex <mr.kailex@gmail.com>
2017-05-08 18:56:53 +0300

    practice3

--- form.php
+++ form.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>Загрузка pdf-файла</title>
-	</head>
+<head>
+    <meta charset="utf-8">
+    <title>Загрузка pdf-файла</title>
+</head>
 <body>
-	<h2>Загрузите свой pdf-файл</h2>
-	<p>(максимальный размер = 5 МB)</p>
-	<br>
-	<form action="upload.php" method="post" enctype="multipart/form-data">
-		<input type="file" name="filename" accept="application/pdf">
-		<br>
-		<input type="submit" value="Загрузить">
-	</form> 
+<h2>Загрузите свой pdf-файл</h2>
+<p>(максимальный размер = 5 МB)</p>
+<br>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="filename" accept="application/pdf">
+    <br>
+    <input type="submit" value="Загрузить">
+</form>
 </body>
 </html>
